b30147970aed6d2f9576b1299f4044fb97f2e102
KyrylIeremieiev <34677@ma-web.nl>
2023-09-07 11:45:59 +0200

    add other 2 outcomes

--- index.php
+++ index.php
@@ -5,8 +5,17 @@ for ($x = 0; $current_number < 544; $x++){
     $current_divBy9 = $current_number / 9;
     if(is_float($current_divBy5) == true and is_float($current_divBy9) == true){
         echo("<b>WEB</b>");
-        continue;
     }
+    elseif (is_float($current_divBy5) == true){
+        echo("<b>SOFTWARE</b>");
+    }
+    elseif (is_float($current_divBy9) == true){
+        echo("<b>DEV</b>");
+    }
+    else{
+        echo($current_number);
+    }
+    $current_number+=1;
 }
 
 ?>

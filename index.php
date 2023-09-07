a469883df247d096d6780b8e1211bab6814575f5
KyrylIeremieiev <34677@ma-web.nl>
2023-09-07 12:30:48 +0200

    add bold filter

--- index.php
+++ index.php
@@ -6,20 +6,28 @@ for ($x = 0; $current_number < 544; $x++){
     //current number is divided by 5 an 9 in different vars, this is so we can check if the current number is divisable by either
     $current_divBy5 = $current_number / 5;
     $current_divBy9 = $current_number / 9;
+    $current_divBy2 = $current_number / 2;
     //if the number is divisable by either number is_fload(that number) will never be a float
     //so we check if it is a float, if it isnt, the code will echo a bold text
     if(is_float($current_divBy5) == false and is_float($current_divBy9) == false){
-        echo("<b>WEB</b>");
+        $current_print="WEB";
     }
     elseif (is_float($current_divBy5) == false){
-        echo("<b>SOFTWARE</b>");
+        $current_print="SOFTWARE";
     }
     elseif (is_float($current_divBy9) == false){
-        echo("<b>DEV</b>");
+        $current_print ="DEV";
     }
     //if both divisions are floats, it will simply show the current  number
     else {
-        echo($current_number);
+        $current_print = $current_number;
+    }
+    //checks for float
+    if(is_float($current_divBy2) == true){
+        echo("<b>{$current_print}</b>");
+    }
+    else{
+        echo($current_print);
     }
     //so that its more clean
     echo("<br>");

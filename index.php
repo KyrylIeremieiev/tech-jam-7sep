045efa5b1732d3588eaf7c7a05db56ca3fa1eae1
KyrylIeremieiev <34677@ma-web.nl>
2023-09-07 12:02:12 +0200

    fix bug (reverse true with false), add <br> between each loop

--- index.php
+++ index.php
@@ -3,18 +3,19 @@ $current_number = 33;
 for ($x = 0; $current_number < 544; $x++){
     $current_divBy5 = $current_number / 5;
     $current_divBy9 = $current_number / 9;
-    if(is_float($current_divBy5) == true and is_float($current_divBy9) == true){
+    if(is_float($current_divBy5) == false and is_float($current_divBy9) == false){
         echo("<b>WEB</b>");
     }
-    elseif (is_float($current_divBy5) == true){
+    elseif (is_float($current_divBy5) == false){
         echo("<b>SOFTWARE</b>");
     }
-    elseif (is_float($current_divBy9) == true){
+    elseif (is_float($current_divBy9) == false){
         echo("<b>DEV</b>");
     }
-    else{
+    else {
         echo($current_number);
     }
+    echo("<br>");
     $current_number+=1;
 }
 

e9d2059c1f000208a8548db0795267f230bc89ee
KyrylIeremieiev <34677@ma-web.nl>
2023-09-07 11:42:46 +0200

    add loop, add outcome if both divisable by 5 and 9

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+$current_number = 33;
+for ($x = 0; $current_number < 544; $x++){
+    $current_divBy5 = $current_number / 5;
+    $current_divBy9 = $current_number / 9;
+    if(is_float($current_divBy5) == true and is_float($current_divBy9) == true){
+        echo("<b>WEB</b>");
+        continue;
+    }
+}
 
 ?>
